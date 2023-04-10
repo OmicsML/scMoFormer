a93ce210d9855b20af72d9094d2df69eee230213
Wenzhuo Tang <tangwen2@msu.edu>
2023-04-10 16:21:01 -0400

    update code to align network with data

--- utils/get_string_network.py
+++ utils/get_string_network.py
@@ -15,6 +15,7 @@ STRING_HUMAN_NETWORK_URL = "https://stringdb-static.org/download/protein.links.v
 STRING_HUMAN_NETWORK_INFO_URL = "https://stringdb-static.org/download/protein.info.v11.5/9606.protein.info.v11.5.txt.gz"
 STRING_HUMAN_NETWORK_ALIAS_URL = "https://stringdb-static.org/download/protein.aliases.v11.5/9606.protein.aliases.v11.5.txt.gz"
 OUT_FILE_NAME = "string_human_ppi_v11.5"
+ALIGNED_PPI_FILE = "string_human_ppi_v11.5_aligned"
 SYMBOL_MAPPING_FILE = "symbol_mapping.csv"
 TARGET_PPI_FILE = "string_human_ppi_v11.5_target"
 GENE_SYMBOL_FILE = "gene_protein_edges"
@@ -45,7 +46,7 @@ def _download():
     return edge_df, info_df
 
 
-def _get_gene_id_map():
+def _get_gene_id_map(subtask, var=None):
     """Obtain mapping from gene ensembl to gene id from data.
 
     The gene id is constructed by joining the gene ensembl and gene symbol
@@ -62,30 +63,34 @@ def _get_gene_id_map():
         ENSG00000203995
 
     """
-    gene_df = None
-    file_names = ["train_cite_inputs.h5ad", "test_cite_inputs.h5ad", "train_multi_targets.h5ad"]
-    for file_name in file_names:
-        path = osp.join(config.RAW_DATA_DIR, file_name)
-        if not osp.isfile(path):
-            raise FileNotFoundError(f"{path} not exist, run utils/data_to_h5ad.py first")
+    if subtask == "cite":
+        gene_df = None
+        file_names = ["train_cite_inputs.h5ad", "test_cite_inputs.h5ad", "train_multi_targets.h5ad"]
+        for file_name in file_names:
+            path = osp.join(config.RAW_DATA_DIR, file_name)
+            if not osp.isfile(path):
+                raise FileNotFoundError(f"{path} not exist, run utils/data_to_h5ad.py first")
 
-        logger.info(f"Loading gene list from {path}")
-        new_df = pd.DataFrame(ad.read_h5ad(path, backed="r").var.index.tolist())[0].str.split("_", expand=True)
-        gene_df = new_df if gene_df is None else gene_df.merge(new_df, how="outer")
-        logger.info(f"\tCurrent number of genes: {gene_df.shape[0]:,}")
-
+            logger.info(f"Loading gene list from {path}")
+            new_df = pd.DataFrame(ad.read_h5ad(path, backed="r").var.index.tolist())[0].str.split("_", expand=True)
+            gene_df = new_df if gene_df is None else gene_df.merge(new_df, how="outer")
+            logger.info(f"\tCurrent number of genes: {gene_df.shape[0]:,}")
+        gene_df.columns = ["gene_ensembl", "gene_symbol"]
+    else:
+        gene_df = var.reset_index()
+        gene_df.columns = ["gene_symbol", "gene_ensembl"]
+        gene_df = gene_df[["gene_ensembl", "gene_symbol"]]
+        
     gene_df.columns = ["gene_ensembl", "gene_symbol"]
     logger.info(f"Total number of genes from data = {gene_df.gene_ensembl.unique().size:,}\n{gene_df}")
-
     gene_id_map = {}  # gene_ensembl -> gene_id
     for _, (ens, sym) in gene_df.iterrows():
         gene_id_map[ens] = "_".join((ens, sym)) if isinstance(sym, str) else ens
 
     return gene_id_map
 
-
 def _convert_and_save(edge_df, info_df, gene_ids, pro_ids, subtask, gene_id_map=None):
-    # Align gene interaction network node ids with data
+    # Map network node ids to gene ids
     edge_df.protein1 = edge_df.protein1.apply(lambda x: x.split(".")[1])
     edge_df.protein2 = edge_df.protein2.apply(lambda x: x.split(".")[1])
     ensps = sorted(set(edge_df.protein1.tolist() + edge_df.protein2.tolist()))
@@ -123,7 +128,6 @@ def _convert_and_save(edge_df, info_df, gene_ids, pro_ids, subtask, gene_id_map=
 
     # Find connections between gene and protein by symbols
     symbol_df = pd.DataFrame(0, index=gene_ids, columns=pro_ids)
-    gene_to_idx = {j: i for i, j in enumerate(gene_ids)}
     gene_series = pd.Series(gene_ids)
     gene_symbols = gene_series.apply(lambda x: x.split("_")[1].upper())
     gene_match_idx = [x for x in range(len(gene_series)) if str(gene_symbols.iloc[x]) in target_symbols]
@@ -150,10 +154,14 @@ def _convert_and_save(edge_df, info_df, gene_ids, pro_ids, subtask, gene_id_map=
         else:
             ensp_to_ensg[ensp] = ensp
     ensp_to_idx = {j: i for i, j in enumerate(ensps)}  # ensp -> index
+    gene_to_idx = {j: i for i, j in enumerate(gene_ids)} # gene id -> index
 
-    converted_edge_df = edge_df.copy()
-    converted_edge_df.protein1 = converted_edge_df.protein1.apply(ensp_to_gid.get)
-    converted_edge_df.protein2 = converted_edge_df.protein2.apply(ensp_to_gid.get)
+    # Align network node ids with data
+    aligned_edge_df = edge_df.copy()
+    aligned_edge_df.protein1 = aligned_edge_df.protein1.apply(ensp_to_gid.get)
+    aligned_edge_df.protein2 = aligned_edge_df.protein2.apply(ensp_to_gid.get)
+    aligned_edge_df = aligned_edge_df[((aligned_edge_df.protein1).isin(gene_ids) & 
+                                       (aligned_edge_df.protein2).isin(gene_ids))]
 
     head = edge_df.protein1.apply(ensp_to_idx.get).values
     tail = edge_df.protein2.apply(ensp_to_idx.get).values
@@ -163,14 +171,19 @@ def _convert_and_save(edge_df, info_df, gene_ids, pro_ids, subtask, gene_id_map=
     target_head = target_pro_ppi.protein1.values
     target_tail = target_pro_ppi.protein2.values
     target_weight = target_pro_ppi.combined_score.values.astype(np.float32)
+    aligned_head = aligned_edge_df.protein1.apply(gene_to_idx.get).values
+    aligned_tail = aligned_edge_df.protein2.apply(gene_to_idx.get).values
+    aligned_weight = aligned_edge_df.combined_score.values.astype(np.float32)
 
     out_path = osp.join(config.PROCESSED_DATA_DIR, f"{OUT_FILE_NAME}.npz")
     np.savez_compressed(out_path, head=head, tail=tail, weight=weight, ids=ids, ensps=ensps, ensgs=ensgs)
     target_path = osp.join(config.PROCESSED_DATA_DIR, f"{TARGET_PPI_FILE}_{subtask}.npz")
     np.savez_compressed(target_path, head=target_head, tail=target_tail, weight=target_weight, ensps=ensps)
+    aligned_path = osp.join(config.PROCESSED_DATA_DIR, f"{ALIGNED_PPI_FILE}_{subtask}.npz")
+    np.savez_compressed(aligned_path, head=aligned_head, tail=aligned_tail, weight=aligned_weight, ids=gene_ids)
     symbol_df.to_hdf(osp.join(config.PROCESSED_DATA_DIR, f"{GENE_SYMBOL_FILE}_{subtask}.h5"), "df")
     logger.info(f"Processed files saved to {config.PROCESSED_DATA_DIR}")
-    return converted_edge_df
+    return aligned_edge_df
 
 def main():
     parser = argparse.ArgumentParser()
@@ -181,12 +194,12 @@ def main():
     pro_info = ad.read_h5ad(osp.join(config.RAW_DATA_DIR, f"train_{subtask}_targets.h5ad"), backed="r")
     if subtask == "cite":
         gene_ids = gene_info.var.index
-        gene_id_map = _get_gene_id_map()
+        gene_id_map = _get_gene_id_map(subtask)
     elif subtask == "gex2adt":
         gene_ensgs = gene_info.var["gene_ids"]
         gene_symbols = [str(x).upper() for x in gene_info.var.index]
         gene_ids = [f"{gene_ensgs[x]}_{gene_symbols[x]}" for x in range(len(gene_ensgs))]
-        gene_id_map = None
+        gene_id_map = _get_gene_id_map(subtask, gene_info.var.gene_ids)
     pro_ids = [str(x).upper() for x in pro_info.var.index]
     edge_df, info_df = _download()
     _convert_and_save(edge_df, info_df, gene_ids, pro_ids, subtask, gene_id_map)
